3b4f62280d9b57214c315e2bcca7028aacfc934d
Wise Colt <wisecolt-macbook@Wise-MacBook-Pro.local>
2019-01-01 06:49:07 +0300

    example update

--- example.js
+++ example.js
@@ -1,13 +1,22 @@
 const SubtitleDetailJSON = require('./index');
+const _ = require('lodash')
 
 SubtitleDetailJSON({
-  nameOrImdb: 'The.End.Of.The.F***ing.World',
-  year: 2017
+  nameOrImdb: 'The Walking Dead',
+  year: 2010
 },(err, res)=>{
   if(err){
     console.log(err);
   }
   else{
-    console.log(res);
+
+      const searchRelease = ''
+      const searchFPS = '23.976'
+      const season = 'S09'
+      const episode = 'E01'
+
+      const result =  _.filter(res, {'fps':searchFPS, 'release': searchRelease, movieOrTv: { 'episode': episode} })
+      console.log(result);
+      console.log(_.maxBy(result, 'downloadRatio'));
   }
 });
